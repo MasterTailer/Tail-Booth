34d0d79bc056b934a14b6bcff7881f099842a068
Ty Poorman <git@flotts.io>
2020-08-01 01:27:50 -0400

    set up webpack!

--- src/hello.js
+++ src/hello.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -6,7 +8,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 let geometry = new THREE.BoxGeometry();
-let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 let cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
