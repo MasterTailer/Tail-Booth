89b35bb85d26dff90fc89882f7ecfeaf9b22ade2
Ty Poorman <git@flotts.io>
2020-08-01 04:10:13 -0400

    rendered fox tail :)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,45 @@
+// JS
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-const scene = new THREE.Scene();
+// CSS
+import './style.css';
+
+let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let geometry = new THREE.BoxGeometry();
-let material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-let cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+let point_light = new THREE.PointLight(0x404040, 5.0); // soft white light
+point_light.translateX(10);
+scene.add(point_light);
 
-camera.position.z = 5;
+let point_light_2 = new THREE.PointLight(0x404040, 5.0); // soft white light
+point_light_2.translateX(-10);
+scene.add(point_light_2);
+
+let loader = new GLTFLoader();
+let tail;
+loader.load('./foxtail.glb',
+    function(obj) {
+        tail = obj.scene;
+        
+        tail.rotateX(0.5);
+        scene.add(tail);
+    },
+    undefined,
+    function(err) {
+        console.error(err);
+    } 
+);
+
+camera.position.z = 3;
 
 function animate() {
     requestAnimationFrame(animate);
-    
-    cube.rotation.y += 0.01;
-    
+    if (tail) tail.rotateY(0.01);
 	renderer.render(scene, camera);
 }
 
